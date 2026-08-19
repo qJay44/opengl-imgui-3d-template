7440fb65e6a3dc5e766473f3ab9aab6822569236
qJay44 <08gerkules08@gmail.com>
2026-08-20 00:08:25 +0300

    nlohmann json

--- include/global.hpp
+++ include/global.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <fstream>
+
 #include "ProfilerManager.hpp"
 #include "glm/ext/matrix_clip_space.hpp"
+#include "nlohmann/json.hpp"
 
 namespace global {
 
@@ -39,6 +42,39 @@ static inline mat4 getScreenProjection() {
   vec2 winSize = getWinSize();
   return glm::ortho(0.f, winSize.x, 0.f, winSize.y);
 }
+
+namespace json {
+  void loadPreset(auto& cfg, std::string_view name) {
+    fspath path = fspath("res/data/cfg") / name;
+
+    std::ifstream f(path);
+
+    if (f.is_open()) {
+      nlohmann::json j;
+      f >> j;
+      j.get_to(cfg);
+      f.close();
+    } else {
+      warning("[global::json::loadPreset] Could not open the file [{}]", path.string());
+    }
+  }
+
+  void savePreset(auto& cfg, std::string_view name) {
+    fspath path = fspath("res/data/cfg") / name;
+    std::filesystem::create_directories(path.parent_path());
+
+    std::ofstream f(path);
+
+    if (f.is_open()) {
+      nlohmann::json j = cfg;
+      f << j.dump(2);
+      f.close();
+    } else {
+      error("[global::json::savePreset] Could not open the file [{}]", path.string());
+    }
+  }
+
+} // namespace json
 
 } // namespace global
 
